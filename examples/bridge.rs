69fe75ec62c677679d0bea73d695f58f13da6332
folkengine <gaoler@electronicpanopticon.com>
2020-09-17 18:36:20 -0400

    added bridge example --- cleanup

--- examples/bridge.rs
+++ examples/bridge.rs
@@ -12,7 +12,7 @@ fn main() {
 }
 
 // "S:Q42.Q52.AQT943.Q 97.AT93.652.T743 AJT85.J76.KJ.A65 K63.K84.87.KJ982"
-pub fn parse_pbn_deal(deck: &cardpack::Pile, s: &str) -> Vec<cardpack::Pile> {
+pub fn parse_pbn_deal(deck: &cardpack::Pile, s: &str) {
     let declarer = s.chars().next().unwrap();
     let directions = compass(declarer);
 
@@ -27,8 +27,6 @@ pub fn parse_pbn_deal(deck: &cardpack::Pile, s: &str) -> Vec<cardpack::Pile> {
 
         println!("{:<5}: {}", dir.to_string(), pile.by_symbol_index());
     }
-
-    vec![]
 }
 
 fn to_pile(deck: &cardpack::Pile, s: &str) -> cardpack::Pile {
