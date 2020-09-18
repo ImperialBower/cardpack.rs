7dc1f5d6228ce80027f57382a167a2ce8037c95c
Christoph <gaoler@electronicpanopticon.com>
2020-09-18 11:11:50 -0400

    Updated license to GNU Affero General Public License 3

--- src/cards/pack.rs
+++ src/cards/pack.rs
@@ -1,7 +1,23 @@
+/*  CardPack - A generic pack of cards library written in Rust.
+Copyright (C) <2020>  Christoph Baker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 use crate::cards::pile::Pile;
 
 /// A Pack is an immutable pile of cards. Packs are designed to be a flexible representation of
-/// a deck, a stack, a discard pile, or a hand.
+/// a deck, stack, discard pile, or hand.
 ///
 /// Packs available are for a traditional 52 card French Deck, pinochle, spades, skat and tarot.
 ///
