0cc53bc5298fd8e0c0467b47abcb83e96df18024
Christoph <gaoler@electronicpanopticon.com>
2020-09-18 11:14:53 -0400

    Added decks module

--- src/cards/decks/bridge.rs
+++ src/cards/decks/bridge.rs
@@ -14,9 +14,3 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-pub mod card;
-pub mod decks;
-pub mod pack;
-pub mod pile;
-pub mod rank;
-pub mod suit;
