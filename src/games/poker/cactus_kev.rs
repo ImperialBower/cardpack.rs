763a28c0dfd79270726538d39fcfa5264c22c9e2
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2021-12-23 12:33:12 -0800

    Refactored CactusKevCard setters

--- src/games/poker/cactus_kev.rs
+++ src/games/poker/cactus_kev.rs
@@ -57,25 +57,30 @@ impl CactusKevCard {
         &self.bites[16..20]
     }
 
+    // #[must_use]
+    // pub fn get_int(&self) -> usize {
+    //     self.bites.as_bitslice().
+    // }
+
     fn set_rank_prime(&mut self, card: &Card) {
         self.bites[26..32].store_be(card.rank.prime);
     }
 
     fn set_rank_flag(&mut self, card: &Card) {
         match card.rank.weight {
-            12 => self.bites[3..4].store(1u8),  // Ace
-            11 => self.bites[4..5].store(1u8),  // King
-            10 => self.bites[5..6].store(1u8),  // Queen
-            9 => self.bites[6..7].store(1u8),   // Jack
-            8 => self.bites[7..8].store(1u8),   // Ten
-            7 => self.bites[8..9].store(1u8),   // Nine
-            6 => self.bites[9..10].store(1u8),  // Eight
-            5 => self.bites[10..11].store(1u8), // Seven
-            4 => self.bites[11..12].store(1u8), // Six
-            3 => self.bites[12..13].store(1u8), // Five
-            2 => self.bites[13..14].store(1u8), // Four
-            1 => self.bites[14..15].store(1u8), // Three
-            0 => self.bites[15..16].store(1u8), // Two
+            12 => self.bites.set(3, true), // Ace
+            11 => self.bites.set(4, true), // King
+            10 => self.bites.set(5, true), // Queen
+            9 => self.bites.set(6, true),  // Jack
+            8 => self.bites.set(7, true),  // Ten
+            7 => self.bites.set(8, true),  // Nine
+            6 => self.bites.set(9, true),  // Eight
+            5 => self.bites.set(10, true), // Seven
+            4 => self.bites.set(11, true), // Six
+            3 => self.bites.set(12, true), // Five
+            2 => self.bites.set(13, true), // Four
+            1 => self.bites.set(14, true), // Three
+            0 => self.bites.set(15, true), // Two
             _ => (),
         }
     }
@@ -86,10 +91,10 @@ impl CactusKevCard {
 
     fn set_suit(&mut self, card: &Card) {
         match card.suit.weight {
-            4 => self.bites[19..20].store(1u8), // Spades
-            3 => self.bites[18..19].store(1u8), // Hearts
-            2 => self.bites[17..18].store(1u8), // Diamonds
-            1 => self.bites[16..17].store(1u8), // Clubs
+            4 => self.bites.set(19, true), // Spades
+            3 => self.bites.set(18, true), // Hearts
+            2 => self.bites.set(17, true), // Diamonds
+            1 => self.bites.set(16, true), // Clubs
             _ => (),
         }
     }
