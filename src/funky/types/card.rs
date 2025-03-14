9e79cc39911d5e3855dd770af80df8af62cd9e95
folkengine <gaoler@electronicpanopticon.com>
2025-03-14 05:52:03 -0700

    funky cards

--- src/funky/types/card.rs
+++ src/funky/types/card.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+use std::str::FromStr;
 use crate::funky::types::mpip::MPip;
 use crate::prelude::Pip;
 use serde::{Deserialize, Serialize};
@@ -15,7 +17,45 @@ pub enum CardType {
     Voucher,
 }
 
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq, Serialize, Deserialize)]
+impl Display for CardType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            CardType::Joker => 'J',
+            CardType::Planet => 'P',
+            CardType::Spectral => 'S',
+            CardType::Tarot => 'T',
+            CardType::Voucher => 'V',
+            _ => '_',
+        };
+        write!(f, "{s}")
+    }
+}
+
+impl From<char> for CardType {
+    fn from(c: char) -> Self {
+        match c {
+            'J' => CardType::Joker,
+            'P' => CardType::Planet,
+            'S' => CardType::Spectral,
+            'T' => CardType::Tarot,
+            'V' => CardType::Voucher,
+            _ => CardType::Basic,
+        }
+    }
+}
+
+impl FromStr for CardType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 1 {
+            return Err(());
+        }
+        Ok(s.chars().next().unwrap().into())
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialOrd, PartialEq, Serialize, Deserialize)]
 pub struct BuffoonCard {
     pub suit: Pip,
     pub rank: Pip,
--- src/funky/types/mod.rs
+++ src/funky/types/mod.rs
@@ -2,3 +2,4 @@ pub mod card;
 pub mod fpips;
 pub mod mpip;
 pub mod score;
+pub mod bpile;
