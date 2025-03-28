6959aa0e064a8d02146d704b1c81fef72118ed78
folkengine <gaoler@electronicpanopticon.com>
2025-03-27 17:57:33 -0700

    rebased from prefunk main

--- src/basic/types/combos.rs
+++ src/basic/types/combos.rs
@@ -174,11 +174,6 @@ impl Combos {
     }
 
     #[must_use]
-    pub fn second(&self) -> Option<&BasicPile> {
-        self.0.get(1)
-    }
-
-    #[must_use]
     pub fn get(&self, position: usize) -> Option<&BasicPile> {
         self.0.get(position)
     }
