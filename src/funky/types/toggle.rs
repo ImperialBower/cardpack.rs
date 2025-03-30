a23ce7e448107f4091d6abc7bf7debd89ce68a34
folkengine <gaoler@electronicpanopticon.com>
2025-03-29 18:27:45 -0700

    Added ToggleCard

--- src/funky/types/toggle.rs
+++ src/funky/types/toggle.rs
@@ -1,5 +1,38 @@
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
+use std::fmt::{Display, Formatter};
+use crate::prelude::Card;
+use crate::preludes::funky::BuffoonCard;
+
+#[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
+pub struct ToggleCard {
+    pub card: BuffoonCard,
+    pub toggle: Toggle,
+}
+
+impl ToggleCard {
+    pub fn new(card: BuffoonCard) -> Self {
+        // Create a new ToggleCard with a default Toggle
+        Self {
+            card,
+            toggle: Toggle::default(),
+        }
+    }
+
+    pub fn is_selected(&self) -> bool {
+        self.toggle.is_on()
+    }
+
+    pub fn toggle(&self) {
+        self.toggle.toggle();
+    }
+}
+
+impl Display for ToggleCard {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.toggle, self.card)
+    }
+}
 
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Toggle {
@@ -8,10 +41,7 @@ pub struct Toggle {
 
 impl Toggle {
     pub fn is_on(&self) -> bool {
-        // Borrow the boolean value inside the RefCell
-        // This will return a boolean indicating if the toggle is on or off
-        let is_on = self.is_on.borrow();
-        *is_on // Dereference to get the actual boolean value
+        *self.is_on.borrow()
     }
 
     pub fn toggle(&self) {
@@ -31,24 +61,54 @@ impl Default for Toggle {
     }
 }
 
+impl Display for Toggle {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let state = if self.is_on() {
+            "+"
+        } else {
+            "-"
+        };
+        write!(f, "{state}")
+    }
+}
+
 #[cfg(test)]
 #[allow(non_snake_case, unused_imports)]
 mod funky__types__toggle_tests {
+    use crate::preludes::funky::ACE_SPADES;
     use super::*;
+
+    #[test]
+    fn toggle_card() {
+        let toggle_card = ToggleCard::new(ACE_SPADES);
+
+        // Check initial state
+        assert!(!toggle_card.is_selected());
+        assert_eq!(toggle_card.to_string(), "-AS");
+
+        // Toggle the state
+        toggle_card.toggle();
+        assert!(toggle_card.is_selected());
+        assert_eq!(toggle_card.to_string(), "+AS");
+
+        // Toggle again
+        toggle_card.toggle();
+        assert!(!toggle_card.is_selected());
+        assert_eq!(toggle_card.to_string(), "-AS");
+    }
 
     #[test]
     fn toggle() {
         let toggle = Toggle::default();
+        assert!(!toggle.is_on());
+        assert_eq!(toggle.to_string(), "-");
 
         toggle.toggle();
         assert!(toggle.is_on());
+        assert_eq!(toggle.to_string(), "+");
 
         toggle.toggle();
         assert!(!toggle.is_on());
-    }
-
-    #[test]
-    fn default() {
-        assert!(!Toggle::default().is_on());
+        assert_eq!(toggle.to_string(), "-");
     }
 }
